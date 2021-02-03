30a0b63617091f27419ea5a2b18d898a37142bab
cwavedave <david@creativewavelength.co.uk>
2021-02-03 14:33:42 +0100

    Designing Quiz

--- ui.py
+++ ui.py
@@ -1,2 +1,14 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzter")
+
+
+
+
+
+        self.window.mainloop()
